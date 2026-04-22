a9b998d0a539f9bf5358bcf72c22bcdac007a7c3
Daniel Stenberg <daniel@haxx.se>
2026-04-22 11:38:02 +0200

    tool_urlglob: add named globs
    Idea-by: Bastian Jesuiter
    
    Verified by test 2408 - 2411
    
    Closes #21409

--- src/tool_urlglob.h
+++ src/tool_urlglob.h
@@ -33,6 +33,7 @@ typedef enum {
 
 struct URLPattern {
   globtype type;
+  char *name;    /* if not NULL */
   int globindex; /* the number of this particular glob or -1 if not used
                     within {} or [] */
   union {
@@ -70,6 +71,11 @@ struct URLGlob {
   const char *error; /* error message */
   size_t pos;        /* column position of error or 0 */
 };
+
+struct URLPattern *glob_find_name(struct URLGlob *glob,
+                                  struct Curl_str *name);
+void glob_show_error(struct URLGlob *glob, const char *url, FILE *error,
+                     CURLcode result);
 
 CURLcode glob_url(struct URLGlob *glob, const char *url, curl_off_t *urlnum,
                   FILE *error);
